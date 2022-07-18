8d5cec6bb2c6e2c34aaf46bb735d243ed0348a74
yilinng <wei9912@gmail.com>
2022-07-18 14:54:16 +0800

    e2e practice

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,5 @@
+require('dotenv-extended').load()
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
@@ -6,7 +8,10 @@ module.exports = defineConfig({
     mochaFile: 'cypress/results/output.xml',
   },
   env: {
-    apiUrl: "https://express-api-react-notion.vercel.app",
+    // HINT: here we read these keys from .env file
+    //, feel free to remove the items that you don't need
+    baseUrl: process.env.CYPRESS_BASE_URL,
+    apiUrl: process.env.REACT_APP_NOT_SECRET_CODE,
     token: "",
     reToken: ""
   },
@@ -14,7 +19,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: process.env.REACT_APP_NOT_SECRET_CODE,
+    baseUrl: "http://localhost:3000",
     specPattern: "cypress/**/*.spec.{js,jsx,ts,tsx}",
   }
 });
